67dbbdc06802ee453ebd50114be07fe52e0ab20c
Amelia Boli <ameliaaboli@gmail.com>
2017-09-20 18:59:27 -0700

    Initial project commit

---
+++ StatusBot/SlackAPIManager.swift
@@ -0,0 +1,15 @@
+//
+//  SlackAPIManager.swift
+//  StatusBot
+//
+//  Created by Amelia Boli on 9/18/17.
+//  Copyright © 2017 Amelia Boli. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class SlackAPIManager {
+
+    
+}
